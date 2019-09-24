d065a36017a97b7ac605cbde9b7e5b5be8f02534
montañistas <j.aguileral@alumnos.duoc.cl>
2019-09-24 13:41:45 -0300

    renovadoxxxx

--- mountainer/mysite/mountain/views.py
+++ mountainer/mysite/mountain/views.py
@@ -3,5 +3,3 @@ from django.shortcuts import render
 def post_list(request):
     return render(request, 'mountain/post_list.html', {})
 	
-def galeria(request):
-    return render(request, 'mountain/galeria.html', {})
