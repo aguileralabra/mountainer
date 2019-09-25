d64caa6eb7ef49ae35c0ae85c0ff8343c373992a
Jordan <aimbox_prox@hotmail.com>
2019-09-25 11:56:35 -0300

    Revert "yei"
    This reverts commit ba235dcae88687d443055f7cff8e66586e3793c1.

--- mountainer/mysite/mountain/urls.py
+++ mountainer/mysite/mountain/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-	path('formulario', views.formulario, name='formulario'),
-    path('galeria', views.galeria, name='galeria'),
-    path('contacto', views.contacto, name='contacto'),
 ]
