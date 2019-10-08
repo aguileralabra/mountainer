e3da3faec5e0455c06d1f020b558e4d5166c461d
Jordan <aimbox_prox@hotmail.com>
2019-10-08 12:32:08 -0300

    Revert "nose q hacer"
    This reverts commit 20475d530aaad35a0212ccaf10436e0961053555.

--- mountainer/mysite/mysite/settings.py
+++ mountainer/mysite/mysite/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'America/Santiago'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -119,4 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
