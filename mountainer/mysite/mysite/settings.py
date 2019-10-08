20475d530aaad35a0212ccaf10436e0961053555
Jordan <aimbox_prox@hotmail.com>
2019-10-08 12:29:34 -0300

    nose q hacer

--- mountainer/mysite/mysite/settings.py
+++ mountainer/mysite/mysite/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
