81a19d9cf0d81ff3904b4cfda797c23653509c75
Jordan <aimbox_prox@hotmail.com>
2019-10-11 02:57:23 -0300

    hola
    bgg

--- mountainer/mysite/mountain/admin.py
+++ mountainer/mysite/mountain/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models import Turist
+from .models import Imagen
 
 admin.site.register(Turist)
--- mountainer/mysite/mountain/models.py
+++ mountainer/mysite/mountain/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 class Turist(models.Model):
-    Nombre = models.CharField(max_length=200)
-    Apellido = models.CharField(max_length=200)
-    Opinion = models.TextField()
+    Nombre = models.CharField(max_length=50, null = True)
+    Apellido = models.CharField(max_length=50, null = True)
+    rut = models.IntegerField( null = True)
+    telefono = models.IntegerField( null = True)
+    pais = models.CharField(max_length=50, default="")
+    lugar = models.CharField(max_length=50, default="")
+    Opinion = models.TextField(null = True )
+
+class Imagen(models.Model):
+    link = models.CharField(max_length=200)
+    Titulo = models.CharField(max_length=20)
+    Descripcion = models.CharField(max_length=20)
 	
 def __str__(self):
         return self.Nombre
