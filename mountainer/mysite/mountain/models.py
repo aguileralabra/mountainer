1883bc26aa436e5a803c1978aa785d1f3a13adc9
Jordan <aimbox_prox@hotmail.com>
2019-10-11 03:32:37 -0300

    bye cache

--- mountainer/mysite/mountain/models.py
+++ mountainer/mysite/mountain/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Turist(models.Model):
     Nombre = models.CharField(max_length=50, null = True)
     Apellido = models.CharField(max_length=50, null = True)
-    telefono = models.IntegerField( null = True)
+    telefono = models.IntegerField(null = True)
     pais = models.CharField(max_length=50, default="")
     lugar = models.CharField(max_length=50, default="")
     Opinion = models.TextField(null = True )
