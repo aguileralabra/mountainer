fafa72d5d363be9fecfd4128df223acacde484a6
Jordan <aimbox_prox@hotmail.com>
2019-11-14 04:12:11 -0300

    diseñando formulario

--- mountainer/mysite/mountain/forms.py
+++ mountainer/mysite/mountain/forms.py
@@ -4,7 +4,49 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class TuristCrear(forms.ModelForm):
+	Nombre = forms.CharField(widget=forms.TextInput(
 
+		attrs={
+			'class': 'form-control',
+			'placeholder': 'Escriba su Nombre'
+		}
+	))
+	Apellido = forms.CharField(widget=forms.TextInput(
+
+		attrs={
+			'class': 'form-control',
+			'placeholder': 'Escriba su Apellido'
+		}
+	))
+	telefono = forms.IntegerField(widget=forms.TextInput(
+
+		attrs={
+			'class': 'form-control',
+			'placeholder': 'Escriba su numero Telefonico'
+		}
+	))
+	pais = forms.CharField(widget=forms.Select(
+
+		attrs={
+			'class': 'form-control',
+			'placeholder': 'Pais en el que vive'
+		}
+	))	
+	lugar = forms.CharField(widget=forms.TextInput(
+
+		attrs={
+			'class': 'form-control',
+			'placeholder': 'Escriba el lugar deseado de ir'
+		}
+	))	
+	Opinion = forms.CharField(widget=forms.Textarea(
+
+		attrs={
+			'class': 'form-control',
+			'placeholder': 'Escriba su opinion',
+			'cols': 20
+		}
+	))
 	class Meta:
 		model = Turist
 
