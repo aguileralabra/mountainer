accc1760518ad9e9246d9e93710ed7cadd6dfc79
Jordan <aimbox_prox@hotmail.com>
2019-11-19 00:19:19 -0300

    test 1 listo

--- mountainer/mysite/mountain/tests.py
+++ mountainer/mysite/mountain/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Turist
+import json
 
-# Create your tests here.
+class TuristTestCase(TestCase):
+
+    def setUp(self):
+        self.turist = Turist()
+        self.response = turist.get(reverse('list'))
+        selft.detail_url = reverse('detail', args=['project1'])
+    
+    def test_project_list_GET(self):
+        response = self.turist.get(self.list_url)
+
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response, 'mountain/turists')
